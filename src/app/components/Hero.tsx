a3e94de6c47d5c074d698705f9b6f1d43e916466
Lucas Legrand <pouetpouets@mbp-de-lucas.home>
2025-01-03 12:22:25 +0100

    typo hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,14 +3,14 @@
 
 export default function Hero() {
   return (
-    <div className="relative isolate px-6 pt-14 lg:px-8">
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+    <div className="relative isolate px-6 pt-10 lg:px-8">
+      <div className="mx-auto max-w-2xl py-30 sm:py-48 lg:py-56">
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
            Trouvez en <span className="bg-yellow-400"> un instant </span> la localisation du bien repéré 
           </h1>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Une extension Chrome pour vous aider à localiser vos recherches immobiliéres, la consomation energétiques, la qualité des isolations et plus encore.
+            Une extension Chrome pour vous aider à localiser vos recherches immobiliéres, connaitre la consomation energétique, la qualité des isolations et plus encore.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
